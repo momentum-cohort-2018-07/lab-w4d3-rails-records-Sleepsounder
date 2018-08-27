1f6f9ae8a9c2ad2b4e275cb10afdc58098923f0a
Geoff <geoff.register@gmail.com>
2018-08-26 22:04:01 -0400

    totes stuck now on something, but finished some basic setup

--- record_collection/app/controllers/albums_controller.rb
+++ record_collection/app/controllers/albums_controller.rb
@@ -1,13 +1,24 @@
 class AlbumsController < ApplicationController
   
-  def index
+#   def index
+#   end
+  def show
+    @album = Album.find(params[:id])
   end
 
   def new
   end
 
   def create
-    render plain: params[:album].inspect
+    @album = Album.new(album_params)
+ 
+    @album.save
+    redirect_to @album
+  end
+
+  private
+    def album_params
+    params.require(:album).permit(:title, :artist)
   end
 
 end
